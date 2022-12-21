d6bc09b8ed57ac0c05056aea4ba9751dd2822041
fuadslymnv <fuadsuleymanov03@gmail.com>
2022-12-21 23:09:24 +0400

    uploading login

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signin from "./pages/Signin";
+import MainLogin from "./components/Login/MainLogin"
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<MainLogin />} />
         <Route path="/signin" element={<Signin />} />
       </Routes>
     </div>
---
+++ src/components/Login/Button.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Button({ content }) {
+  return <StyledButton>{content}</StyledButton>;
+}
+
+const StyledButton = styled.button`
+  background: rgba(23, 26, 32, 0.8);
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  width: 65%;
+  height: 3rem;
+  border: none;
+  color: white;
+  border-radius: 2rem;
+  cursor: pointer;
+`;
